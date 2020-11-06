0da84daa19dfecdcf37df72e1383513a9a6e74c2
Hussein <hmedeking@gmail.com>
2020-11-06 23:52:16 +0100

    stats.out

--- stats.c
+++ stats.c
@@ -1,21 +1,28 @@
+
+
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief C file of the first assessments
  *
- * <Add Extended Description Here>
+ * A Simple C program file of the first assessments that calculate some statistics and print them on screen :
+ *     -find_median
+ *     -find_mean
+ *     -find_maximum
+ *     -find_minimum
+ *     -sort_array
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Hussein HAMADI
+ * @date 6-11-2020
  *
  */
 
@@ -24,20 +31,110 @@
 #include <stdio.h>
 #include "stats.h"
 
-/* Size of the Data Set */
-#define SIZE (40)
 
-void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+int  main()
+{
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+	int test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+			114, 88,   45,  76, 123,  87,  25,  23,
+			200, 122, 150, 90,   92,  87, 177, 244,
+			201,   6,  12,  60,   8,   2,   5,  67,
+			7,  87, 250, 230,  99,   3, 100,  90};
 
+
+	print_statistics(test,SIZE);
+	return 0;
 }
 
-/* Add other Implementation File Code Here */
+
+
+int find_minimum(int *ptr , int count)
+{
+	int i=0;
+	int min=ptr[0];
+	for (i=1;i<count;i++)
+	{
+		if (ptr[i]<min)
+		{
+			min=ptr[i];
+		}
+	}
+	return min;
+}
+int find_maximum(int *ptr , int count)
+{
+	int i=0;
+	int max=ptr[0];
+	for (i=1;i<count;i++)
+	{
+		if (ptr[i]>max)
+		{
+			max=ptr[i];
+		}
+	}
+	return max;
+}
+int find_mean(int * ptr, int count)
+{
+	int i;
+	int sum=0;
+	for(i =0;i<count;i++)
+	{
+		sum =sum + ptr[i];
+	}
+	return (sum/count);
+}
+void sort_array(int * ptr, int count)
+{
+	int key,j;
+	for(int i=1;i<count;i++)
+	{
+		key = ptr[i];
+		j=i-1;
+		while(j>=0 && ptr[j]<key)
+		{
+			ptr[j+1]=ptr[j];
+			j=j-1;
+		}
+		ptr[j+1]=key;
+	}
+}
+int find_median(int * ptr, int count)
+{
+	int i,median,first_number ,second_number;
+	sort_array(ptr,count);
+	if(count%2 == 0)
+	{
+		i = count/2;
+		first_number=ptr[i-1];
+		second_number=ptr[i];
+		median = (first_number+second_number)/2;
+	}
+	else
+	{
+		i = count/2;
+		median = ptr[i-1];
+	}
+	return median;
+}
+
+void print_array(int * ptr, int count)
+{
+	for(int i=0;i<count;i++)
+	{
+		printf("%d",ptr[i]);
+	}
+}
+
+void print_statistics(int * ptr, int count)
+{
+	int min = find_minimum(ptr,count);
+	int max = find_maximum(ptr,count);
+	int medain = find_median(ptr,count);
+	int mean = find_mean(ptr,count);
+	printf("The minimum number of array = %d\n",min);
+	printf("The maximum number of array = %d\n",max);
+	printf("The median number of array = %d\n",medain);
+	printf("The mean number of array = %d\n",mean);
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,116 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief header file of the first assessments
  *
- * <Add Extended Description Here>
+ * A header file of the first assessments that contain prototypes of functions:
+ *     -print_statistics
+ *     -print_array
+ *     -find_median
+ *     -find_mean
+ * 	   -find_maximum
+ *     -find_minimum
+ *     -sort_array
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Hussein HAMADI
+ * @date 6-11-2020
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Size of the Data Set */
+#define SIZE (40)
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+ /******************************************************************************
+ * Function: print_statistics
+ * Description:
+ *     This function prints the statistics of an array including minimum, maximum, mean, and median.
+ * Parameters:
+ *     int * ptr: Pointer to a data set
+ *     int count: Number of items in data set
+ * Return:
+ *     void
+ *****************************************************************************/
+ 
+void print_statistics(int * ptr, int count);
+
+/******************************************************************************
+* Function: print_array
+* Description:
+*     This function takes an array of data and a length, prints the array to the screen
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     void
+*****************************************************************************/
+void print_array(int * ptr, int count);
+
+/******************************************************************************
+* Function: find_median
+* Description:
+*     This function takes an array of data and a length, returns the median value
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     int median : the median value
+*****************************************************************************/
+int find_median(int * ptr, int count);
+
+/******************************************************************************
+* Function: find_mean
+* Description:
+*     This function takes an array of data and a length, returns the mean
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     int mean : the mean value
+*****************************************************************************/
+int find_mean(int * ptr, int count);
+
+/******************************************************************************
+* Function: find_maximum
+* Description:
+*     This function takes an array of data and a length, returns the maximum value
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     int max : the maximum value
+*****************************************************************************/
+
+int find_maximum(int * ptr, int count);
+ 
+/******************************************************************************
+* Function: find_minimum
+* Description:
+*     This function takes an array of data and a length, returns the minimum value
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     int min : the minimum value
+*****************************************************************************/
+
+int find_minimum(int * ptr, int count);
+
+/******************************************************************************
+* Function: sort_array
+* Description:
+*     an array of data and a length, sorts the array from largest to smallest.
+*     (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+* Parameters:
+*     int * ptr: Pointer to a data set
+*     int count: Number of items in data set
+* Return:
+*     void
+*****************************************************************************/
+
+void sort_array(int * ptr, int count);
 
 
 #endif /* __STATS_H__ */
